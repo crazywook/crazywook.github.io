bbe81dc02063fc050ad1da20d3b3c7437758d1c0
swkim <swkim@fin2b.com>
2020-04-08 12:03:33 +0900

    remove ignored folder

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Typescript를 이용한 node server 만들기",
-        "excerpt":"Welcome hsahaha dependencies ```json “dependencies”: { “@hapi/joi”: “^17.1.1”, “body-parser”: “^1.19.0”, “class-transformer”: “^0.2.3”, “class-validator”: “^0.10.2”, “config”: “^3.3.1”, “continuation-local-storage”: “^3.2.1”, “cookie-parser”: “~1.4.4”, “cors”: “^2.8.5”, “debug”: “~2.6.9”, “express”: “~4.16.1”, “http-errors”: “~1.6.3”, “multer”: “^1.4.2”, “mysql2”: “^2.1.0”, “nunjucks”: “^3.2.0”, “reflect-metadata”: “^0.1.13”, “routing-controllers”: “^0.8.0”, “sequelize”: “^5.21.5”, “sequelize-typescript”: “^1.1.0”, “typescript”: “^3.8.3” }, “devDependencies”: { “@types/body-parser”: “^1.19.0”, “@types/bluebird”:...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/typescript-server/",
-        "teaser": null
-      }]
